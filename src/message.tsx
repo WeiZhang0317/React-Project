4c2cbc946e9c04ab1eef3e13641d19485c58cfd0
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2024-11-29 17:33:26 +1300

    list group component add

--- src/message.tsx
+++ src/message.tsx
@@ -1,9 +1,10 @@
 function Message(){
   const name ='' ;
   if (name)
-  //jsx: javascript xml       
+  //jsx: javascript xml， {name} 并不是一个完整的 JavaScript 语句，而是 JSX 语法中的 JavaScript 表达式。  
+
      return <h1>Hello {name}</h1>
-  return <h1>Hello world</h1>
+  return <h1>Hello world123</h1>
 }
 
 export default Message;
